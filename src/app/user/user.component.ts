aa95de6150382f8c8f416fa9ce78e0711667f102
unknown <kiranchenagala@gmail.com>
2018-07-09 14:25:20 +0530

    initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector : 'app-user',
+    templateUrl : './user.template.html' ,
+    styles : [
+        `h1{
+            color: red;
+        }
+    `]
+})
+
+export class UserComponent{}    
